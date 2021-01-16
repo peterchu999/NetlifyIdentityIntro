5bedc9371ab53e210747eecc16f8831839634567
peterchu999 <peter.andrew987@gmail.com>
2021-01-16 16:14:54 +0700

    init

---
+++ src/components/route-login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RouteLogin = () => (
+  <>
+    <h1>Login/SignUp</h1>
+    <button>LogIn</button>
+  </>
+);
+
+export default RouteLogin;
---
+++ src/components/route-secret.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const RouteSecret = () => <h1>All your secret list</h1>;
+
+export default RouteSecret;
